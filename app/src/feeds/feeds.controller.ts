9393be2a7a974cb300c37f8531708ac943cc8f02
Jorge Martínez Bastida <jorgemartinezbastida@Air-de-Jorge.home>
2021-01-31 10:03:15 +0100

    feat: 🎸 CRUD operations

--- app/src/feeds/feeds.controller.ts
+++ app/src/feeds/feeds.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('feeds')
-export class FeedsController {}
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+  } from '@nestjs/common';
+  import { ApiTags } from '@nestjs/swagger';
+  import { FeedDto } from './dto/feed.dto';
+  import { FeedsService } from './feeds.service';
+  import { Feed } from './schemas/feed.schema';
+  
+  @ApiTags('Feeds')
+  @Controller('feeds')
+  export class FeedsController {
+    constructor(private readonly feedsService: FeedsService) {}
+  
+    @Get()
+    async findFeeds(): Promise<Feed[]> {
+      return await this.feedsService.findFeeds();
+    }
+  
+    @Post()
+    async createFeed(@Body() createFeedDto: FeedDto): Promise<void> {
+      await this.feedsService.createFeed(createFeedDto);
+    }
+  
+    @Put(':feedId')
+    async editFeed(
+      @Param('feedId')
+      id: string,
+      @Body() updateFeedDto: FeedDto,
+    ): Promise<void> {
+      await this.feedsService.editFeed(id, updateFeedDto);
+    }
+  
+    @Delete(':feedId')
+    async removeFeed(
+      @Param('feedId')
+      id: string,
+    ): Promise<void> {
+      await this.feedsService.deleteFeed(id);
+    }
+  }
+  
--- app/src/feeds/feeds.service.ts
+++ app/src/feeds/feeds.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { FeedDto } from './dto/feed.dto';
+import { Feed, FeedDocument } from './schemas/feed.schema';
 
 @Injectable()
-export class FeedsService {}
+export class FeedsService {
+  constructor(
+    @InjectModel(Feed.name) private readonly feedModel: Model<FeedDocument>,
+    private readonly httpClient: HttpService,
+  ) {}
+
+  async findFeeds(): Promise<Feed[]> {
+    return this.feedModel.find().exec();
+  }
+
+  async createFeed(createFeedDto: FeedDto): Promise<Feed> {
+    const createdFeed = new this.feedModel(createFeedDto);
+    return createdFeed.save();
+  }
+
+  async editFeed(feedId: string, updateFeedDto: FeedDto): Promise<void> {
+    await this.feedModel.updateOne(
+      {
+        _id: feedId,
+      },
+      updateFeedDto,
+    );
+  }
+
+  async deleteFeed(feedId: string) {
+    await this.feedModel.deleteOne({ _id: feedId });
+  }
+}
