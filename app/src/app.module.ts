e6aa4849243aa48de3097e2131f96388a5a90a5b
Jorge Martínez Bastida <jorgemartinezbastida@Air-de-Jorge.home>
2021-01-31 09:51:15 +0100

    ci: 🎡 Api in repo

---
+++ app/src/app.module.ts
@@ -0,0 +1,13 @@
+
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { FeedsModule } from './feeds/feeds.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://mongo/feeds'), FeedsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
